0fc6a969de9a6c7e1efcdc371b5a3c9b3e6b925b
Janhavi Parulekar <parulekar.j@husky.neu.edu>
2018-08-06 03:54:42 -0400

    Mlab connected

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds241737.mlab.com:41737/todos' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
