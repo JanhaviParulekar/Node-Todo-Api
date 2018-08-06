c60d2487dc2edce39017d25ec85c4e873526d729
Janhavi Parulekar <parulekar.j@husky.neu.edu>
2018-08-06 01:05:27 -0400

    Mongoose queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {Users} = require('./../server/models/user');
+
+// var id = '5b67cc09e55d6a182a39406100';
+
+// if(!ObjectID.isValid(id)){
+//     console.log('Id not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) =>{
+//     console.log('Todos: ', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) =>{
+//     console.log('Todo: ', todo);
+// });
+
+// Todo.findById(id).then((todoById) =>{
+//     if(!todoById){
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by id: ', todoById);
+// }).catch((e) => console.log(e));
+
+
+var userId = '5b64fbbdc3cd05f43ac87386';
+
+Users.findById(userId).then((user) => {
+    if(!user){
+        return console.log('User not found');
+    }
+    console.log('User: ', user);
+}).catch((e) => console.log(e));
