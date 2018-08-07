46b2b81b11cbe72c0fd5bc891bea5be0ecdb095c
Janhavi Parulekar <parulekar.j@husky.neu.edu>
2018-08-06 21:56:06 -0400

    Commented

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-process.env.MONGODB_URI = 'mongodb://parulekar.j:Americanme94@ds241737.mlab.com:41737/todos';
+// process.env.MONGODB_URI = 'mongodb://parulekar.j:Americanme94@ds241737.mlab.com:41737/todos';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
