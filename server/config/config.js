91f64f91a1d33b460116101c15800808af070720
Janhavi Parulekar <parulekar.j@husky.neu.edu>
2018-08-14 22:10:24 -0400

    code commented

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ if(env === 'development' || env === 'test'){
 //     process.env.PORT =3000;
 //     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest' ;
 // // }
- else if (env === 'prod') {
-    process.env.MONGODB_URI
-    // process.env.MONGODB_URI = 'mongodb://parulekar.j:Americanme94@ds119422.mlab.com:19422/todoapp';
-}
+//  else if (env === 'prod') {
+//     process.env.MONGODB_URI
+//     // process.env.MONGODB_URI = 'mongodb://parulekar.j:Americanme94@ds119422.mlab.com:19422/todoapp';
+// }
